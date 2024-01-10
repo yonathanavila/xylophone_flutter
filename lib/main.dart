3dd5577d5f13ec7169f4b50b73f62fafda8c3293
Yonathan Cruz <yonathancruz2019@gmail.com>
2024-01-09 21:04:21 -0600

    add wrap button component

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void playSound(int sound) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$sound.wav'));
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -18,18 +23,131 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.resolveWith(
-                      (states) => Colors.blueAccent)),
-              onPressed: () async {
-                final player = AudioPlayer();
-                await player.play(AssetSource('note1.wav'));
-              },
-              child: Text('Click on me'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                        side: const BorderSide(color: Colors.black),
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.all(Colors.red),
+                  ),
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                        side: const BorderSide(color: Colors.black),
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.all(Colors.orange),
+                  ),
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                        side: const BorderSide(color: Colors.black),
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.all(Colors.yellow),
+                  ),
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                        side: const BorderSide(color: Colors.black),
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.all(Colors.green),
+                  ),
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                        side: const BorderSide(color: Colors.black),
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.all(Colors.teal),
+                  ),
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                        side: const BorderSide(color: Colors.black),
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  ),
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  child: Text(''),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.zero,
+                        side: const BorderSide(color: Colors.black),
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.all(Colors.purple),
+                  ),
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  child: Text(''),
+                ),
+              ),
+            ],
           ),
         ),
       ),
